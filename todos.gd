dbaa63532b800709b2ca536970f60a3a3a98e09c
aftrosrs <dylanwbannerman@gmail.com>
2024-06-28 03:26:20 -0500

    nothing important except todos.gd, no actual code

--- todos.gd
+++ todos.gd
@@ -1,4 +1,9 @@
 class_name Todos 
 
-#TODO more questions, try to get 15
+
+
+#TODO ill try to figure out more questions, maybe to get 15, maybe just 10.
+#idk may try to animate buttons pressed
+#idk may try to make a sound effect for button press
+
 
