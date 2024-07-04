d76fdba4f4c18267b055d03bb675c0dedfb62655
aftrosrs <dylanwbannerman@gmail.com>
2024-07-04 03:37:38 -0500

    Update todos.gd
    #FIXME UPDATE FONT
    #pseudo font is hard to read.
    
    #broken fix pact with the devil, possible state machine as a fix.

--- todos.gd
+++ todos.gd
@@ -1,8 +1,11 @@
 class_name Todos 
 
 
+#FIXME UPDATE FONT
+#pseudo font is hard to read.
+
+#broken fix pact with the devil, possible state machine as a fix.
 
 #idk may try to animate buttons pressed
 #idk may try to make a sound effect for button press
-
 
